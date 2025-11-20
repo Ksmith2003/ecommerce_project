0c0040dbd977c6d11ac63371400813421fdca618
Kayla Smith <kaymsmith2003@gmail.com>
2025-11-20 13:30:15 -0600

    added image uploads to admin dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  belongs_to :category
+
+  has_one_attached :image
+  def self.ransackable_attributes(auth_object = nil)
+    [ "category_id", "category_name", "created_at", "description", "id", "price", "product_name", "updated_at" ]
+  end
+  def self.ransackable_associations(auth_object = nil)
+    [ "category", "image" ]
+  end
+
+  validates :product_name, presence: true
 end
