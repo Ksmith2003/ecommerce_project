33e598416f9e9c6e71bb8697b3322244cf7b23ca
Kayla Smith <kaymsmith2003@gmail.com>
2025-11-28 14:36:07 -0600

    added redirect to cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,6 +2,8 @@ class CartController < ApplicationController
   def add
     session[:cart] << params[:product_id]
     flash[:notice] = "Added the item to the cart"
+
+    redirect_to products_path
   end
 
   def destroy
