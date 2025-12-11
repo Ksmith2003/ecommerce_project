6087c5e3b820af1a5e770b6baa045f9f636ffd0c
Kayla Smith <kaymsmith2003@gmail.com>
2025-12-11 13:21:03 -0600

    added the rest of the collumns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_28_204223) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_11_192018) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.integer "author_id"
     t.string "author_type"
@@ -92,9 +92,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_204223) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "country"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "province"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
